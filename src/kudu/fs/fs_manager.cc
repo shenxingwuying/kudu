4ac0b26be94a259a0dd516855043585b84d4052b
Andrew Wong <awong@cloudera.com>
2021-12-14 11:48:44 -0800

    [KUDU-76][consensus] force fsync metadata on XFS
    We've seen issues like KUDU-2195 rear their heads more prominently on
    XFS, sometimes requiring costly surgery to fix by restoring metadata if
    all replicas lose their consensus metadata.
    
    This patch adds to the band-aid flag --cmeta_force_fsync, checking if
    the metadata is on an XFS mount, and if so, always fsyncing. This new
    behavior is enabled by default with the --cmeta_fsync_override_on_xfs
    flag.
    
    Change-Id: Ie97138e3522f20325be104d3665d887693b79a10
    Reviewed-on: http://gerrit.cloudera.org:8080/18101
    Reviewed-by: Alexey Serbin <aserbin@cloudera.com>
    Tested-by: Kudu Jenkins
    Reviewed-by: Attila Bukor <abukor@apache.org>

--- src/kudu/fs/fs_manager.cc
+++ src/kudu/fs/fs_manager.cc
@@ -60,6 +60,14 @@
 
 DECLARE_int32(log_container_sample_count);
 
+DEFINE_bool(cmeta_fsync_override_on_xfs, true,
+            "Whether to ignore --cmeta_force_fsync and instead always flush if Kudu detects "
+            "the server is on XFS. This can prevent consensus metadata corruption in the "
+            "event of sudden server failure. Disabling this flag may cause data loss in "
+            "the event of a system crash. See KUDU-2195 for more details.");
+TAG_FLAG(cmeta_fsync_override_on_xfs, experimental);
+TAG_FLAG(cmeta_fsync_override_on_xfs, advanced);
+
 DEFINE_bool(enable_data_block_fsync, true,
             "Whether to enable fsync() of data blocks, metadata, and their parent directories. "
             "Disabling this flag may cause data loss in the event of a system crash.");
@@ -158,7 +166,8 @@ FsManager::FsManager(Env* env, FsManagerOpts opts)
   : env_(DCHECK_NOTNULL(env)),
     opts_(std::move(opts)),
     error_manager_(new FsErrorManager()),
-    initted_(false) {
+    initted_(false),
+    meta_on_xfs_(false) {
   DCHECK(opts_.update_instances == UpdateInstanceBehavior::DONT_UPDATE ||
          !opts_.read_only) << "FsManager can only be for updated if not in read-only mode";
 }
@@ -342,10 +351,23 @@ Status FsManager::PartialOpen(CanonicalizedRootsList* missing_roots) {
           metadata_->uuid(), pb->uuid()));
     }
   }
-
   if (!metadata_) {
     return Status::NotFound("could not find a healthy instance file");
   }
+  const auto& meta_root_path = canonicalized_metadata_fs_root_.path;
+  const auto bad_meta_fs_msg =
+      Substitute("Could not determine file system of metadata directory $0",
+                 meta_root_path);
+  Status s = env_->IsOnXfsFilesystem(meta_root_path, &meta_on_xfs_);
+  if (FLAGS_cmeta_fsync_override_on_xfs) {
+    // Err on the side of visibility if we expect a behavior change based on
+    // the file system.
+    RETURN_NOT_OK_PREPEND(s, bad_meta_fs_msg);
+  } else {
+    WARN_NOT_OK(s, bad_meta_fs_msg);
+  }
+  VLOG(1) << Substitute("Detected metadata directory is $0on an XFS mount: $1",
+                        meta_on_xfs_ ? "" : "not ", meta_root_path);
   return Status::OK();
 }
 
