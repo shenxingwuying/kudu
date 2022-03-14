e3f589fb480daa5441f4028f6fa392192b4aff60
dengke <dengke@sensorsdata.cn>
2022-03-14 11:45:44 +0800

    [SD][KUDU-127] unify the use of tools in different environments

--- soku-construction-blueprint/step/soku_tool_installer_step.py
+++ soku-construction-blueprint/step/soku_tool_installer_step.py
@@ -6,6 +6,7 @@ from hyperion_utils.shell_utils import check_call
 sys.path.append(os.path.join(os.environ['SENSORS_PLATFORM_HOME'], '..', 'armada', 'hyperion'))
 from construction_vehicle.step.base_installer_step import BaseInstallerStep
 
+
 class SokuToolInstallerStep(BaseInstallerStep):
     def __init__(self):
         super().__init__()
@@ -13,21 +14,30 @@ class SokuToolInstallerStep(BaseInstallerStep):
         self.package_dir = os.path.join(os.path.dirname(__file__), '..', '..')
 
     def update(self):
-        # 更新系统目录下kudu工具的链接位置到soku_tool目录下，以便kudu工具进行小版本升级
+        # 更新系统目录下kudu工具的链接位置到soku_tool目录下
         dir_kudu = os.path.join("/usr/bin", "kudu")
         if os.path.islink(dir_kudu):
             cmd = "sudo rm -f '%s'" % dir_kudu
             check_call(cmd, self.logger.debug)
-            src_kudu = os.path.join(self.product_home, "soku_tool/kudu")
-            cmd = "sudo ln -s '%s' '%s'" % (src_kudu, dir_kudu)
-            check_call(cmd, self.logger.debug)
+        src_kudu = os.path.join(self.product_home, "soku_tool/kudu")
+        cmd = "sudo ln -s '%s' '%s'" % (src_kudu, dir_kudu)
+        check_call(cmd, self.logger.debug)
         dir_sp_kudu = os.path.join("/usr/bin", "sp_kudu")
         if os.path.islink(dir_sp_kudu):
             cmd = "sudo rm -f '%s'" % dir_sp_kudu
             check_call(cmd, self.logger.debug)
-            src_sp_kudu = os.path.join(self.product_home, "soku_tool/sp_kudu")
-            cmd = "sudo ln -s '%s' '%s'" % (src_sp_kudu, dir_sp_kudu)
-            check_call(cmd, self.logger.debug)        
+        src_sp_kudu = os.path.join(self.product_home, "soku_tool/sp_kudu")
+        cmd = "sudo ln -s '%s' '%s'" % (src_sp_kudu, dir_sp_kudu)
+        check_call(cmd, self.logger.debug)
+        # CDH环境下kudu会链接到该目录下，且直接替换的方式并不能使/usr/bin/kudu的优先级更高
+        # 保险起见，直接将这里的kudu链接到soku_tool目录下,升级时直接替换源soku_tool目录即可
+        cdh_tool_home = os.path.join("/sensorsdata/main/cloudera/parcels/CDH/bin/", "kudu")
+        if os.path.islink(cdh_tool_home) or os.path.isfile(cdh_tool_home):
+            cmd = "sudo rm -f '%s'" % cdh_tool_home
+            check_call(cmd, self.logger.debug)
+            soku_tool_kudu = os.path.join(self.product_home, "soku_tool/kudu")
+            cmd = "sudo ln -s '%s' '%s'" % (soku_tool_kudu, cdh_tool_home)
+            check_call(cmd, self.logger.debug)
 
     def check(self):
         # 适配 installer 通用流程，不需要校验
---
+++ soku-upgrader/step/soku_tool_upgrader_step.py
@@ -0,0 +1,63 @@
+#!/bin/env python
+# -*- coding: UTF-8 -*-
+
+import os
+import sys
+
+sys.path.append(os.path.join(os.environ['SENSORS_PLATFORM_HOME'], '..', 'armada', 'hyperion'))
+from upgrader.step.dir_step import DirStep
+import utils.shell_wrapper
+
+
+class SokuToolUpgraderStep(DirStep):
+    def __init__(self):
+        super().__init__()
+        self.package_dir = self.root_path
+        self.logger.info('soku_tool update, package_dir is %s' % self.package_dir)
+        self.package_tar = ''
+        for file in os.listdir(self.package_dir):
+            if file.startswith('soku_tool-') and file.endswith('.tar'):
+                self.package_tar = file
+                break
+        self.update_tar_full_path = os.path.join(self.package_dir, self.package_tar)
+
+    def update(self):
+        # 更新文件和版本号
+        self.update_files()
+        self.change_image_tag_to_current()
+        soku_home = os.environ['SENSORS_SOKU_HOME']
+        # 先将对应版本的soku_tool替换
+        cmd = "tar xvf '%s' -C '%s'" % (self.update_tar_full_path, soku_home)
+        utils.shell_wrapper.check_call(cmd, self.logger.debug)
+        # 更新系统目录下kudu工具的链接位置到soku_tool目录下
+        dir_kudu = os.path.join("/usr/bin", "kudu")
+        if os.path.islink(dir_kudu):
+            cmd = "sudo rm -f '%s'" % dir_kudu
+            utils.shell_wrapper.check_call(cmd, self.logger.debug)
+        src_kudu = os.path.join(soku_home, "soku_tool/kudu")
+        cmd = "sudo ln -s '%s' '%s'" % (src_kudu, dir_kudu)
+        utils.shell_wrapper.check_call(cmd, self.logger.debug)
+        dir_sp_kudu = os.path.join("/usr/bin", "sp_kudu")
+        if os.path.islink(dir_sp_kudu):
+            cmd = "sudo rm -f '%s'" % dir_sp_kudu
+            utils.shell_wrapper.check_call(cmd, self.logger.debug)
+        src_sp_kudu = os.path.join(soku_home, "soku_tool/sp_kudu")
+        cmd = "sudo ln -s '%s' '%s'" % (src_sp_kudu, dir_sp_kudu)
+        utils.shell_wrapper.check_call(cmd, self.logger.debug)
+        # CDH环境下kudu会链接到该目录下，且直接替换的方式并不能使/usr/bin/kudu的优先级更高
+        # 保险起见，直接将这里的kudu链接到soku_tool目录下,升级时直接替换源soku_tool目录即可
+        cdh_tool_home = os.path.join("/sensorsdata/main/cloudera/parcels/CDH/bin/", "kudu")
+        if os.path.islink(cdh_tool_home) or os.path.isfile(cdh_tool_home):
+            cmd = "sudo rm -f '%s'" % cdh_tool_home
+            utils.shell_wrapper.check_call(cmd, self.logger.debug)
+            cmd = "sudo ln -s '%s' '%s'" % (src_kudu, cdh_tool_home)
+            utils.shell_wrapper.check_call(cmd, self.logger.debug)
+
+    def rollback(self):
+        # 1.把backup目录下的文件全部拷贝回去
+        self.restore_files()
+        self.rollback_image_tag()
+
+    def check(self):
+        # 适配 upgrader 通用流程，不需要校验
+        return True
